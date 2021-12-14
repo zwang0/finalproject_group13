7d8dc4173ec542e8004d2b8b4bb7ca9faac0b6f2
FayeFang17 <78106382+FayeFang17@users.noreply.github.com>
2021-12-13 23:47:48 -0500

    Update trees_and_forests.R

--- trees_and_forests.R
+++ trees_and_forests.R
@@ -1,3 +1,6 @@
+
+
+#Random Forest
 library(rpart)
 library(rattle)
 library(rpart.plot)
@@ -8,25 +11,27 @@ library(randomForest)
 tree.hitters<-rpart(Rented.Bike.Count~.,
                     method="anova",
                     cp=0.02,
-                    data=testset)
+                    data=trainset)
 fancyRpartPlot(tree.hitters,sub="")
 
-n<-nrow(testset)
+n<-nrow(trainset)
 rss_here <- rep(0,n) 
 for(i in 1:n){
   tree.hittersCV <-rpart(Rented.Bike.Count~.,
                          method="anova",
                          cp=0.02,
-                         data=testset[-i,])
+                         data=trainset[-i,])
 
-  yhat <- predict(tree.hittersCV, newdata = testset[i,])
+  yhat <- predict(tree.hittersCV, newdata = trainset[i,])
   
-  rss_here[i] <- (testset$Rented.Bike.Count[i] - yhat)^2
-  }
-  
+  rss_here[i] <- (trainset$Rented.Bike.Count[i] - yhat)^2
+}
+#RMSE for train
 sqrt(mean(rss_here))
-
-
+#for testset
+tree.pred <- predict(tree.hitters, testset[,-1])
+tree.MSE <- crossprod(tree.hitters - testset[,1]) / length(testindex)
+sqrttree.MSE)
 
 #bagged Forest
 
@@ -35,7 +40,7 @@ bag.bike<-randomForest(Rented.Bike.Count~.,
                        mtry=12,
                         importance=TRUE,
                         ntree=1000,
-                        data=testset,
+                        data=trainset,
                         compete=FALSE)
 bag.bike
 #CER:percent increase in OOB error
@@ -43,17 +48,20 @@ dotchart(importance(bag.bike)[,1])
 #percent increase in Gini index
 dotchart(importance(bag.bike)[,2])
 #impact of variable  on count
-partialPlot(bag.bike,testset,x.var="Hour",ylab="Count")
-partialPlot(bag.bike,testset,x.var="Temperature..C.",ylab="Count")
-partialPlot(rf.bike,testset,x.var="Humidity",ylab="Count")
-partialPlot(rf.bike,testset,x.var="Solar.Radiation..MJ.M2.",ylab="Count")
-partialPlot(bag.bike,testset,x.var="Functioning.Day",ylab="Count")
-partialPlot(bag.bike,testset,x.var="Seasons",ylab="Count")
-partialPlot(bag.bike,testset,x.var="Snowfall..cm.",ylab="Count")
+partialPlot(bag.bike,trainset,x.var="Hour",ylab="Count")
+partialPlot(bag.bike,trainset,x.var="Temperature..C.",ylab="Count")
+partialPlot(rf.bike,trainset,x.var="Humidity",ylab="Count")
+partialPlot(rf.bike,trainset,x.var="Solar.Radiation..MJ.M2.",ylab="Count")
+partialPlot(bag.bike,trainset,x.var="Functioning.Day",ylab="Count")
+partialPlot(bag.bike,trainset,x.var="Seasons",ylab="Count")
+partialPlot(bag.bike,trainset,x.var="Snowfall..cm.",ylab="Count")
 #RMSE
 predict.OOB<-bag.bike$predicted
-sqrt(mean((testset$Rented.Bike.Count-predict.OOB)^2))
-
+sqrt(mean((trainset$Rented.Bike.Count-predict.OOB)^2))
+#for testset
+bag.pred <- predict(bag.bike, testset[,-1])
+bag.MSE <- crossprod(bag.pred - testset[,1]) / length(testindex)
+sqrt(rf.MSE)
 
 #random Forest
 
@@ -62,7 +70,7 @@ rf.bike<-randomForest(Rented.Bike.Count~.,
                        mtry=sqrt(12),
                        importance=TRUE,
                        ntree=1000,
-                       data=testset,
+                       data=trainset,
                        compete=FALSE)
 rf.bike
 #CER:percent increase in OOB error
@@ -70,14 +78,19 @@ dotchart(importance(rf.bike)[,1])
 #percent increase in Gini index
 dotchart(importance(rf.bike)[,2])
 #impact of variable  on count
-partialPlot(rf.bike,testset,x.var="Hour",ylab="Count")
-partialPlot(rf.bike,testset,x.var="Temperature..C.",ylab="Count")
-partialPlot(rf.bike,testset,x.var="Humidity",ylab="Count")
-partialPlot(rf.bike,testset,x.var="Functioning.Day",ylab="Count")
-partialPlot(rf.bike,testset,x.var="Solar.Radiation..MJ.M2.",ylab="Count")
+partialPlot(rf.bike,trainset,x.var="Hour",ylab="Count")
+partialPlot(rf.bike,trainset,x.var="Temperature..C.",ylab="Count")
+partialPlot(rf.bike,trainset,x.var="Humidity",ylab="Count")
+partialPlot(rf.bike,trainset,x.var="Functioning.Day",ylab="Count")
+partialPlot(rf.bike,trainset,x.var="Solar.Radiation..MJ.M2.",ylab="Count")
 
-partialPlot(rf.bike,testset,x.var="Seasons",ylab="Count")
+partialPlot(rf.bike,trainset,x.var="Seasons",ylab="Count")
 
-#RMSE
+#RMSE for traningset 
 predict.OOB.1<-rf.bike$predicted
-sqrt(mean((testset$Rented.Bike.Count-predict.OOB.1)^2))
+sqrt(mean((trainset$Rented.Bike.Count-predict.OOB.1)^2))
+
+#for testset
+rf.pred <- predict(rf.bike, testset[,-1])
+rf.MSE <- crossprod(rf.pred - testset[,1]) / length(testindex)
+sqrt(rf.MSE)
